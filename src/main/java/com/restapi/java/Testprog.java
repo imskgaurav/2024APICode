182d95200e352b54cea2dd9cd8957d79872d3afa
imskgaurav <shashikant.gaurav@gmail.com>
2024-11-25 23:24:03 +0530

    created Api Framework

---
+++ src/main/java/com/restapi/java/Testprog.java
@@ -0,0 +1,11 @@
+package com.restapi.java;
+
+public class Testprog {
+
+    public static void main(String[] args) {
+        System.out.println("Run Java");
+
+
+    }
+    
+}
---
+++ src/test/com/prob/javacode/Leetcode_2707.java
@@ -0,0 +1,7 @@
+public class Leetcode_2707 {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
